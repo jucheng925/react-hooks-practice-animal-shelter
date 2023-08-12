813a7324e75219db5090734cbd9368f51b9fc566
Julie C <jucheng925@gmail.com>
2023-08-11 20:25:33 -0400

    finish practice problems

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,36 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  const updateFilters = (newType) => setFilters({type: newType}) 
+  
+  const fetchPets =() => {
+    if (filters.type === "all") {
+      fetch("http://localhost:3001/pets")
+        .then(resp => resp.json())
+        .then(data => setPets(data))
+      }
+    else fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+  }
+
+  const updateAdopted = (id) => {
+    const newPetArray = pets.map((pet)=> {
+      if (pet.id === id) {
+        return {
+          ...pet,
+          isAdopted: true,
+        };
+      } else {
+        return pet;
+      }
+    });
+    setPets(newPetArray);
+  }
+
+  // This callback should take in an id for a pet, find the matching pet in the 
+  // pets array in App, and set the isAdopted property to true.
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +45,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={updateFilters} onFindPetsClick={fetchPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={updateAdopted}/>
           </div>
         </div>
       </div>
